94b42753f4afa90ed3769c2389271d67d6dad702
Matthew Berthoud <matthewberthoud@gmail.com>
2025-05-01 15:05:34 -0400

    benchmark script

---
+++ benchmarks.sh
@@ -0,0 +1,12 @@
+csv_file="benchmark-results.csv"
+txt_file="benchmark-results.txt"
+
+go test -bench=. > "$txt_file"
+echo "Raw output written to $txt_file"
+echo "method,ms_per_op" > "$csv_file"
+cat benchmark-results.txt | awk '/ns\/op/ { 
+    gsub(/^Benchmark/, "", $1);    # Remove "Benchmark" from the start
+    gsub(/-8$/, "", $1);           # Remove "-8" from the end
+    printf "%s,%.3f\n", $1, $3/1000000 
+}' >> "$csv_file"
+echo "CSV output written to $csv_file"
