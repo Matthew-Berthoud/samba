5efc282f086106ec5f9fbf6f1207f9060d0af7a2
Matthew Berthoud <matthewberthoud@gmail.com>
2025-05-01 16:02:09 -0400

    renamed

--- evaluation/benchmarks.sh
+++ evaluation/benchmarks.sh
@@ -1,5 +1,5 @@
-csv_file="benchmark-results.csv"
-txt_file="benchmark-results.txt"
+csv_file="bench.csv"
+txt_file="bench.txt"
 
 cd ../
 go test -bench=. > "evaluation/$txt_file"
@@ -7,7 +7,7 @@ cd evaluation
 echo "Raw output written to $txt_file"
 
 echo "method,ms_per_op" > "$csv_file"
-cat benchmark-results.txt | awk '/ns\/op/ { 
+cat bench.txt | awk '/ns\/op/ { 
     gsub(/^Benchmark/, "", $1);    # Remove "Benchmark" from the start
     gsub(/-8$/, "", $1);           # Remove "-8" from the end
     printf "%s,%.3f\n", $1, $3/1000000 
